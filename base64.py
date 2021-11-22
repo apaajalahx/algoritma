77dd3f8e0868db65171f0eeaf9d66d5a605871ea
Dinar Hamid <52324987+apaajalahx@users.noreply.github.com>
2021-11-22 22:29:08 +0700

    Create base64.py

---
+++ base64.py
@@ -0,0 +1,23 @@
+#!/bin/python3
+
+table = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/'
+
+def base64_encode(data):
+    d = str()
+    for z in data:
+        d += bin(int('0x' + z.encode('utf-8').hex(), 0)).replace('b','')
+    while len(d) % 3:
+        d += '00000000'
+    for z in range(6, len(d) + int(len(d)/6), 7):
+        d = d[:z] + "|" + d[z:]
+    d = d.split('|')
+    z = ''
+    for u in d:
+        if u == '000000':
+            z += '='
+        else:
+            if u != '':
+                z += table[int(u, 2)]
+    print(z)
+
+base64_encode('test')
