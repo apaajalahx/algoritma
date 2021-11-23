30a59558a7d0cb4b9292ca51ff71cc710f44c796
Dinar Hamid <52324987+apaajalahx@users.noreply.github.com>
2021-11-23 10:01:23 +0700

    Update base64.py
    kalo ada 'titik/spasi/spesial karakter' bakal error wkwkwk

--- base64.py
+++ base64.py
@@ -1,7 +1,6 @@
 #!/bin/python3
 
 table = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/'
-
 def base64_encode(data):
     d = str()
     for z in data:
@@ -20,4 +19,28 @@ def base64_encode(data):
                 z += table[int(u, 2)]
     return z
 
-base64_encode('test')
+def base64_decode(data):
+    d = str()
+    for z in data:
+        if z == '=':
+            break; continue
+        d += '{:0>6}'.format(bin(table.index(z)).replace('0b',''))
+    for z in range(8, len(d) + int(len(d)/8), 9):
+        d = d[:z] + '|' + d[z:]
+    u = str()
+    for z in d.split('|'):
+        if z != '':
+            if not len(z) < 6:
+                u += chr(int(z, 2))
+    return u
+
+
+def test():
+    t1 = 'admin'
+    t1_enc = base64_encode(t1)
+    print(t1_enc)
+    t2_dec = base64_decode(t1_enc)
+    if t2_dec == t1:
+        print('OK')
+
+test()
